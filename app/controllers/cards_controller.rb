3f3bab152e0fc1121a8ef211c241b561c93ea62d
strettos <strettos@yandex.ru>
2014-09-11 10:09:11 +0400

    start task3

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,20 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def  create
+    @card = Card.new(card_params)
+
+    @card.save
+    redirect_to @card
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:original_text, :translated_text, :review_date)
+    end
 end
