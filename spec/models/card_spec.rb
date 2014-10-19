c3f7f8a7885a51702a432799dce200c7acc3e7a9
Nadya Malakhova <strettos@yandex.ru>
2014-10-19 18:39:32 +0400

    task 5 add can update test

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 
 describe Card do
-  
+
  it 'can translate' do
   card = Card.new(original_text: "hause", translated_text: "дом")
   expect(card.check_answer("врач")).to be false
@@ -11,6 +11,12 @@ describe Card do
  it 'correct translate' do
   card = Card.new(original_text: "hause", translated_text: "дом")
   expect(card.check_answer("дом")).to be true
+ end
+
+ it 'can update' do
+  card = Card.new(original_text: "hause", translated_text: "дом", review_date: "2014.01.01")
+  card.set_review_date
+  expect(card.review_date).to eq(Date.today + 3.days)
  end
 
 end
