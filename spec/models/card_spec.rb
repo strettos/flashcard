4f85f24c1d5d8af2ea5c5922b059f5ca6d35bedb
Nadya Malakhova <strettos@yandex.ru>
2014-10-20 20:10:01 +0400

    task5 convert to spaces

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 describe Card do
 
-	it 'can translate' do
-		card = Card.new(original_text: "hause", translated_text: "дом")
-		expect(card.check_answer("врач")).to be false
- 	end
+  it 'can translate' do
+    card = Card.new(original_text: "hause", translated_text: "дом")
+    expect(card.check_answer("врач")).to be false
+  end
 
- 	it 'correct translate' do
-		card = Card.new(original_text: "hause", translated_text: "дом")
-		expect(card.check_answer("дом")).to be true
- 	end
- 	
- 	context '#change_review_date' do
- 		it 'updates review date correctly' do
-			card = Card.new(original_text: "hause", translated_text: "дом", review_date: "2014.01.01")
-			card.set_review_date
-			expect(card.review_date).to eq(Date.today + 3.days)
- 		end
- 	end
+  it 'correct translate' do
+    card = Card.new(original_text: "hause", translated_text: "дом")
+    expect(card.check_answer("дом")).to be true
+  end
+  
+  context '#change_review_date' do
+    it 'updates review date correctly' do
+      card = Card.new(original_text: "hause", translated_text: "дом", review_date: "2014.01.01")
+      card.set_review_date
+      expect(card.review_date).to eq(Date.today + 3.days)
+    end
+  end
 end
