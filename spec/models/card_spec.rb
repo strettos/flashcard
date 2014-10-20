6a1700d5deb80b088a1db9f3cec45f325aee1798
Nadya Malakhova <strettos@yandex.ru>
2014-10-21 01:02:29 +0400

    task5 fix

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -12,20 +12,18 @@ describe Card do
       card = Card.new(original_text: "hause", translated_text: "дом")
       expect(card.check_answer("дом")).to be true
     end
+
+    it 'updates review date when correct answer' do
+      card = Card.new(original_text: "hause", translated_text: "дом")
+      card.check_answer("дом")
+      expect(card.review_date).to eq(Date.today + 3.days)
+    end
   end
   
   context '#change_review_date' do
     it 'updates review date correctly' do
       card = Card.new(original_text: "hause", translated_text: "дом", review_date: "2014.01.01")
       card.set_review_date
-      expect(card.review_date).to eq(Date.today + 3.days)
-    end
-  end
-
-  context '#review' do
-    it 'updates review date when correct answer' do
-      card = Card.new(original_text: "hause", translated_text: "дом")
-      card.check_answer("дом")
       expect(card.review_date).to eq(Date.today + 3.days)
     end
   end
